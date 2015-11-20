e00ae2ed57df594f96d731d5383bedad29a9c6d4
langenoir <Ln1878@langenoirs-MacBook-Pro.local>
2015-11-20 13:53:44 -0600

    copied everything from MP2

---
+++ Guest-login.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * This page will provide instruction for testing the application
+ * Login page will navigate to setup and sutomatically prepare the database
+ * added session control on Nov 19, 2015
+ * 16:49:51 pm @ PS 3001- 718
+ */
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+<meta http-equiv="Content-Type" content="text/html" charset="UTF-8">
+
+<style>
+.left_align{
+	margin-left: 47px;
+	width: 98%;
+   // border:1px solid grey;
+}
+.bgImage{
+	position:fixed;
+	margin-top:30px;
+	margin-left:417px;
+	margin-right: 70px;
+	margin-bottom: 30px;
+}
+</style>
+
+
+
+<head>
+	<meta charset="UTF-8">
+	<title>Login</title>
+<link rel="stylesheet" type="text/css" href="stylesheet.css" title="Style">
+
+</head>
+<body background = "black">
+
+<div class = "bgImage">
+ <img src="wall2.png" height="600" width="900">
+</div>
+
+<div class="left_align">
+	<br>
+		<form action="Guest-index.php" method="POST">
+			<h1><font color = "white"> Welcome! Guest!</font></h1>
+			<br>
+			
+			<font color = "white">
+				<h1>Testing Instruction:</h1><br>
+				<p>This is the guest page</p>
+				<p>The guest system won't automatically subscribe to this application</p><br>
+				<p>This page will automatically prepare the database for user</p>
+				<p>Please <a href= "login.php"> Log In </a> & subscribe! </p><br>
+				<p><font color = "grey"> ITMP 544 MP-2 15Fall </font></p>
+				<br>
+				<br><br><br>
+			</font>
+			
+			<input type="submit" value = "Continue as Guest">
+			<br>
+		</form>
+</div>
+
+</body>
+</html>
---
+++ simpleLaunch.sh
@@ -0,0 +1,33 @@
+# testing simple launch without autoscaling
+aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --security-group-ids $4 --subnet-id $5 --key-name $6  --associate-public-ip-address --iam-instance-profile Name=$7 --user-data file://../MP2Environment-setup/install-webserver.sh --debug
+
+#testing database connection
+aws rds create-db-instance --db-name simmoncatdb --db-instance-identifier simmon-the-cat-db --db-instance-class db.t1.micro --engine mysql --master-username LN1878 --master-user-password hesaysmeow --allocated-storage 5
+
+# MP2 test
+# aws sns create-topic --name mp2 
+
+#wait
+echo -e "\nPlease wait for a few minute, creating database : simmoncatdb . . ."
+
+aws rds wait db-instance-available --db-instance-identifier simmon-the-cat-db
+
+echo -e "\n Finished creating the database."
+
+# db read-only replica
+#echo "\n Creating read-only replica . . ."
+
+#aws rds create-db-instance-read-replica --db-instance-identifier simmon-the-cat-db-read-only --source-db-instance-identifier simmon-the-cat-db 
+
+#echo "Read-only replica created: simmon-the-cat-db-read-only "
+
+#skipping the manual setup process
+#echo "\nSetting up database for testing, please wait . . . "
+
+#sudo php ../MP2Application-setup/setup.php
+
+
+#echo -e "\nDone! Please navigate to index.php in the web browser bar for testing. "
+
+# cloudwatch, updated Nov 19th, 2015 ---to be added into launch.sh
+# aws cloudwatch put-metric-alarm --metric-name SIMMON-METRIC --alarm-name SIMMON-ALARM --alarm-description "SIMMON-ALARM triggered! " --namespace AWS/EC2 --dimensions Name=SIMMON-AUTO-SCALE,Value=SIMMON-AUTO-SCALE --statistic Average  --metric-name CPUUtilization --comparison-operator GreaterThanOrEqualToThreshold --threshold 30 --period 360 --evaluation-periods 4 -- alarm-actions arn:(----to be added---)
