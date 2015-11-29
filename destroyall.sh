2e30c6cf1bbdc75f03ff8ac296c1b5f5c9ac49a3
langenoir <Ln1878@langenoirs-MacBook-Pro.local>
2015-11-28 22:18:47 -0600

    added cleanup content, fully functional destroyall.sh

--- destroyall.sh
+++ destroyall.sh
@@ -3,6 +3,12 @@
 declare -a s3ARR
 declare -a s3CountARR
 
+
+declare -a cleanupARR
+declare -a cleanupLBARR
+declare -a dbInstanceARR
+
+
 #aws s3 rb s3://* --force
 #aws s3 ls | grep bucket-name
 mapfile -t s3CountARR < <(aws s3 ls)
@@ -26,3 +32,87 @@ sleep 1
 done
 
 echo "All s3 buckets deleted associated with this project."
+sleep 3
+
+#added cleanup.sh
+
+aws ec2 describe-instances --filter Name=instance-state-code,Values=16 --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g"
+
+mapfile -t cleanupARR < <(aws ec2 describe-instances --filter Name=instance-state-code,Values=16 --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g")
+
+echo "the output is ${cleanupARR[@]}"
+
+aws ec2 terminate-instances --instance-ids ${cleanupARR[@]} 
+
+echo "-Cleaning up existing Load Balancers"
+mapfile -t cleanupLBARR < <(aws elb describe-load-balancers --output json | grep LoadBalancerName | sed "s/[\"\:\, ]//g" | sed "s/LoadBalancerName//g")
+
+echo "The LBs are ${cleanupLBARR[@]}"
+
+LENGTH=${#cleanupLBARR[@]}
+echo "ARRAY LENGTH IS $LENGTH"
+for (( i=0; i<${LENGTH}; i++)); 
+  do
+  aws elb delete-load-balancer --load-balancer-name ${cleanupLBARR[i]} --output text
+  sleep 1
+done
+
+# Delete existing RDS  Databases
+# Note if deleting a read replica this is not your command 
+mapfile -t dbInstanceARR < <(aws rds describe-db-instances --output json | grep "\"DBInstanceIdentifier" | sed "s/[\"\:\, ]//g" | sed "s/DBInstanceIdentifier//g" )
+
+if [ ${#dbInstanceARR[@]} -gt 0 ]
+   then
+   echo "-Deleting existing RDS database-instances"
+   LENGTH=${#dbInstanceARR[@]}  
+
+   # http://docs.aws.amazon.com/cli/latest/reference/rds/wait/db-instance-deleted.html
+      for (( i=0; i<${LENGTH}; i++));
+      do 
+      aws rds delete-db-instance --db-instance-identifier ${dbInstanceARR[i]} --skip-final-snapshot --output text
+      aws rds wait db-instance-deleted --db-instance-identifier ${dbInstanceARR[i]} --output text
+      sleep 1
+   done
+fi
+
+# Create Launchconf and Autoscaling groups
+
+LAUNCHCONF=(`aws autoscaling describe-launch-configurations --output json | grep LaunchConfigurationName | sed "s/[\"\:\, ]//g" | sed "s/LaunchConfigurationName//g"`)
+
+SCALENAME=(`aws autoscaling describe-auto-scaling-groups --output json | grep AutoScalingGroupName | sed "s/[\"\:\, ]//g" | sed "s/AutoScalingGroupName//g"`)
+
+echo "The asgs are: " ${SCALENAME[@]}
+echo "the number is: " ${#SCALENAME[@]}
+
+if [ ${#SCALENAME[@]} -gt 0 ]
+  then
+echo "SCALING GROUPS to delete, please wait..."
+#aws autoscaling detach-launch-
+
+#aws autoscaling delete-auto-scaling-group --auto-scaling-group-name ${SCALENAME[@]}
+
+#aws autoscaling delete-launch-configuration --launch-configuration-name ${LAUNCHCONF[@]}
+
+#aws autoscaling update-auto-scaling-group --auto-scaling-group-name $SCALENAME --min-size 0 --max-size 0
+
+aws autoscaling update-auto-scaling-group --auto-scaling-group-name $SCALENAME --min-size 0 --max-size 0 --desired-capacity 0
+
+aws autoscaling  disable-metrics-collection --auto-scaling-group-name $SCALENAME
+
+
+aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $SCALENAME --force-delete
+
+
+aws autoscaling delete-launch-configuration --launch-configuration-name $LAUNCHCONF
+
+fi
+
+
+echo "Successfully destroyed everything associated with this project!"
+
+
+
+
+
+
+
