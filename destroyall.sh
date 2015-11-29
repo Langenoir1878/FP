aa1064810201a997f2adb7f80be2ce62bbbfe855
langenoir <Ln1878@langenoirs-MacBook-Pro.local>
2015-11-28 22:05:39 -0600

    destroy multiple

--- destroyall.sh
+++ destroyall.sh
@@ -1,20 +1,26 @@
 #!/usr/local/bin/bash
 
 declare -a s3ARR
+declare -a s3CountARR
 
 #aws s3 rb s3://* --force
 #aws s3 ls | grep bucket-name
-aws s3api list-buckets --query 'Buckets[].Name'
+#mapfile -t s3CountARR < <(aws s3 ls)
+LENGTH=${#s3CountARR[@]}
+
+echo "ARRAY LENGTH IS $LENGTH"
+
+
+aws s3api list-buckets --bucket 'Buckets[].Name'
 #aws s3 ls --query 'Buckets[].Name'
-mapfile -t s3ARR < <(aws s3api list-buckets --query 'Buckets[].Name')
+mapfile -t s3ARR < <(aws s3api list-buckets --bucket 'Buckets[].Name')
 #mapfile -t s3ARR < <(aws s3 ls --query 'Buckets[].Name')
 
 echo "The current s3 buckets are ${s3ARR[@]}"
 
-LENGTH=${#s3ARR[@]}
-echo "ARRAY LENGTH IS $LENGTH"
 
 aws s3 rb s3://${s3ARR[@]} --force --output text
 sleep 1
+
 
 echo "All s3 buckets deleted associated with this project."
